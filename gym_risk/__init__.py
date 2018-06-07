5f8c70626cd8ee21c306f2d233c91af55fc67c5c
Léo Andéol <leo.andeol@gmail.com>
2018-06-07 16:09:44 +0200

    Template files

---
+++ gym_risk/__init__.py
@@ -0,0 +1,6 @@
+from gym.envs.registration import register
+
+register(
+    id='Risk-v0'
+    entry_point='gym_risk.envs:RiskEnv'
+)
---
+++ gym_risk/envs/risk_env.py
@@ -0,0 +1,18 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+class RiskEnv(gym.Env):
+    metadata = {'render.modes' = ['human']}
+
+    def __init(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self, mode='human', close=False):
+        pass
