696a5e9b4f5a2b26c991a26060aa7b074dfe8b7d
Léo Andéol <leo.andeol@gmail.com>
2018-06-21 14:40:15 +0200

    Fixed pip related issue

--- gym_risk/__init__.py
+++ gym_risk/__init__.py
@@ -1,5 +1,7 @@
 from gym.envs.registration import register
 
+name="gym_risk"
+
 register(
     id='Risk-v0',
     entry_point='gym_risk.envs:RiskEnv',
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as f:
 
 setup(
     name="gym_risk",
-    version="0.0.1",
+    version="0.0.2",
     install_requires=["gym"],
     author="Léo Andéol",
     autor_email="leo.andeol@gmail.com",
