008483590cbe7f5c4d910c7b54aa2ee1e0065636
Léo Andéol <leo.andeol@gmail.com>
2018-06-21 14:56:40 +0200

    Another minor fix

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md","r") as f:
     long_description = f.read()
 
 setup(
     name="gym_risk",
-    version="0.0.2",
+    version="0.0.3",
     install_requires=["gym"],
     author="Léo Andéol",
     author_email="leo.andeol@gmail.com",
     description="An OpenAI Gym wrapper for the Risk game",
     long_description=long_description,
     url="https://github.com/leoandeol/gym-risk/",
+    packages=find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
